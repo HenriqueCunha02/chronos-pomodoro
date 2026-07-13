97f44ef40c823dd668cb20acd7cbe194abf23273
Henrique Cunha <henriqueartusi39@gmail.com>
2026-07-13 14:50:53 -0300

    Add Context and Provider

---
+++ src/contexts/TaskContext/TaskContext.tsx
@@ -0,0 +1,17 @@
+import { createContext } from 'react';
+import { initialTaskState } from './initialTaskState';
+import type { TaskStateModel } from '../../models/TaskStateModel';
+
+export type TaskContextProps = {
+  state: TaskStateModel;
+  setState: React.Dispatch<React.SetStateAction<TaskStateModel>>;
+};
+
+const initialContextValue = {
+  state: initialTaskState,
+  setState: () => {},
+};
+
+export const TaskContext = createContext<TaskContextProps>(initialContextValue);
+
+
